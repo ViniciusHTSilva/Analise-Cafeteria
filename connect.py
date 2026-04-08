eaec1308210f17867e9407f218a3abe2d30b7663
vinicius <viniciushtsilva@gmail.com>
2026-04-08 00:20:39 -0300

    Adicionando .env para melhorar seguranca do projeto.

--- connect.py
+++ connect.py
@@ -1,9 +1,17 @@
+import os
 import psycopg2
 
+from dotenv import load_dotenv
+load_dotenv()
+
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_DATABASE = os.getenv("POSTGRES_DB")
 
 conexao = psycopg2.connect("dbname=postgres",
-                           user="postgres",
-                            password="postgres",
-                           host="localhost",
-                           port="5433")
+                            user=DB_USER,
+                            password=DB_PASSWORD,
+                            dbname=DB_DATABASE,
+                            host="localhost",
+                            port="5432")
 cursor = conexao.cursor()
