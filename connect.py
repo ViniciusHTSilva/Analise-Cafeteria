fc4938ca52f58416f7072d12cc1edfd87a0ee113
vinicius <viniciushtsilva@gmail.com>
2026-04-23 00:29:55 -0300

    Fazendo pesquisas no ano 2025

--- connect.py
+++ connect.py
@@ -8,11 +8,11 @@ DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_DATABASE = os.getenv("POSTGRES_DB")
 
-conexao = psycopg2.connect("dbname=postgres",
+conn = psycopg2.connect("dbname=postgres",
                             user=DB_USER,
                             password=DB_PASSWORD,
                             dbname=DB_DATABASE,
                             host="localhost",
                             port="5432")
-cursor = conexao.cursor()
+cursor = conn.cursor()
 print("Conectado com sucesso")
