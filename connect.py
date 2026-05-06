8336dd77295bc41f5b11a523099b5c59079ea019
vinicius <viniciushtsilva@gmail.com>
2026-05-06 00:47:17 -0300

    Adicionando previsao de faturamento 2025

--- connect.py
+++ connect.py
@@ -4,15 +4,16 @@ import psycopg2
 from dotenv import load_dotenv
 load_dotenv()
 
-DB_USER = os.getenv("POSTGRES_USER")
+DB_USER = os.getenv("F")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_DATABASE = os.getenv("POSTGRES_DB")
+DB_HOST = os.getenv("POSTGRES_HOST")
 
 conn = psycopg2.connect("dbname=postgres",
                             user=DB_USER,
                             password=DB_PASSWORD,
                             dbname=DB_DATABASE,
-                            host="localhost",
+                            host=DB_HOST,
                             port="5432")
 cursor = conn.cursor()
 print("Conectado com sucesso")
